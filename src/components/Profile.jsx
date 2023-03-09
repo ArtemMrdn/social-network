90ab5e5b33694c0de7457a5d394e6dc091407ba0
ArtomMrdn <artommrdn47@gmail.com>
2023-03-09 11:49:46 +0200

    lesson 12 added

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7qpwVVDL5JxcranO9YRjfs4WG4hliTEh4IA44dKd2zqXcpp72HuV6BfncHV59FQ6NnwQ&usqp=CAU' />
+      </div>
+      <div>ava+description</div>
+      <div>
+        My post
+        <div>New post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
