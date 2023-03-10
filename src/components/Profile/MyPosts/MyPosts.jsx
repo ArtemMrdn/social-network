b53b0925af7e71b8a73c53e5d7b8cc4042b58668
ArtomMrdn <artommrdn47@gmail.com>
2023-03-10 12:41:08 +0200

    lesson 16-17 added

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,10 +9,8 @@ const MyPosts = () => {
       <button>Add post</button>
       <button>Remove</button>
       <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message='Hi, how are you?' count='10' />
+        <Post message="It's my first post" count='12' />
       </div>
     </div>
   );
