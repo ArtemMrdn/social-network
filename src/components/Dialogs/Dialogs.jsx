2575ccd934ff3474535d2332dda49f86ff640c7c
ArtomMrdn <artommrdn47@gmail.com>
2023-03-13 12:06:03 +0200

    lesson 21 added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,21 @@
-import "./Dialogs.module.css";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={`${s.dialog} + ${s.active}`}>Artem</div>
+        <div className={s.dialog}>Daniel</div>
+        <div className={s.dialog}>Valera</div>
+        <div className={s.dialog}>Sanek</div>
+      </div>
+      <div className={s.messages} s>
+        <div className={s.message}>Hi </div>
+        <div className={s.message}>Hi, hru</div>
+        <div className={s.message}>Hi everyone</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
