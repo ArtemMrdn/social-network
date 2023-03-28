131d6f66df36f7113cd1baba9b6f5dad80022fb0
ArtomMrdn <artommrdn47@gmail.com>
2023-03-28 20:40:01 +0300

    lesson 45 added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import * as serviceWorker from "./serviceWorker";
 import store from "./redux/redux-store";
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "./StoreContext";
+import { Provider } from "react-redux";
 
 let rerenderEntireTree = () => {
   ReactDOM.render(
@@ -23,9 +21,3 @@ rerenderEntireTree();
 store.subscribe(() => {
   rerenderEntireTree();
 });
-
-// API
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
