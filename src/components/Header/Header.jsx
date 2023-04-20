a3de0bf5d020058cb331b46748bbe305894e8c74
ArtomMrdn <artommrdn47@gmail.com>
2023-04-20 11:42:03 +0300

    lesson 61 added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
-      <img src='https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr' />
+      <img
+        src='https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr'
+        alt='Header'
+      />
+
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
