2ff1fb2aa0131cfbbc526249421a54b0c07049ec
ArtomMrdn <artommrdn47@gmail.com>
2023-05-05 10:10:33 +0300

    lesson 79 added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,15 @@ const Header = (props) => {
         src='https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr'
         alt='Header'
       />
-
+      ;
       <div className={s.loginBlock}>
-        {props.isAuth ? props.login : <NavLink to={"login"}>Login</NavLink>}
+        {props.isAuth ? (
+          <div>
+            {props.login} - <button onClick={props.logout}>Log out</button>{" "}
+          </div>
+        ) : (
+          <NavLink to={"/login"}>Login</NavLink>
+        )}
       </div>
     </header>
   );
