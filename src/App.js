fb3331c292e459198123b1d9a246e57f829a9edd
ArtomMrdn <artommrdn47@gmail.com>
2023-06-01 11:49:38 +0300

    lesson 95 added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Route, withRouter } from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import { connect, Provider } from "react-redux";
@@ -12,6 +10,14 @@ import { compose } from "redux";
 import { initializeApp } from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
+import { withSuspense } from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
 
 class App extends Component {
   componentDidMount() {
@@ -28,9 +34,12 @@ class App extends Component {
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={() => <DialogsContainer />} />
+          <Route path='/dialogs' render={withSuspense(DialogsContainer)} />
 
-          <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+          <Route
+            path='/profile/:userId?'
+            render={withSuspense(ProfileContainer)}
+          />
 
           <Route path='/users' render={() => <UsersContainer />} />
 
